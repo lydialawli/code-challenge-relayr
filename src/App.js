5f807f61cacd614f53ecb8a2c1acd0f9d9f53eeb
lydialawli <lydialawli@gmail.com>
2019-12-19 20:31:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className='instructions'>
+            <h1>Relayr Device Dashboard</h1>
+            <p>Feel free to implement UI the way you like.</p>
+        </div>
+    );
+}
+
+export default App;
