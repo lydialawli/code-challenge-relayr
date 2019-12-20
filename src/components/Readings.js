065068a56b2928360f1b172d62ef50d36af9c05b
lydialawli <lydialawli@gmail.com>
2019-12-20 16:36:08 +0100

    show all readings plus some basic styling

--- src/components/Readings.js
+++ src/components/Readings.js
@@ -1,18 +1,38 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
+const DisplayReading = (props) => {
+    return (
+        <div className="displayReading">
+            <h2>{props.reading.name}</h2>
+            <h2>{props.reading.value}</h2>
+            <h2>{props.reading.unit}</h2>
+            <h2>{props.reading.timestamp}</h2>
+            <h2>{props.reading.acive}</h2>
+        </div>
+    )
+}
+
 const Readings = () => {
     const data = useSelector((state) => state.data)
     const dispatch = useDispatch();
 
+    const titles = {
+        name: 'NAME',
+        unit: 'UNIT',
+        value: 'VALUE',
+        timestamp: 'TIMESTAMP',
+        active: 'STATUS'
+    }
     // useEffect(() => {
     //     console.log(data.data)
     // })
 
     return (
         <div >
+            <DisplayReading reading={titles}></DisplayReading>
             {data.map((d) => (
-                <h2 key={d.name}>{d.name}</h2>
+                <DisplayReading key={d.name} reading={d} ></DisplayReading>
             ))}
         </div>
     )
